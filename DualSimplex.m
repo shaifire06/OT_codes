15cf7fa30ea69a8a28bcff3dbe087999b3dfc895
Shaifali Thakur <100794287+shaifire06@users.noreply.github.com>
2023-04-24 02:30:52 +0530

    Create DualSimplex.m

---
+++ DualSimplex.m
@@ -0,0 +1,60 @@
+%dual simplex
+Z=[-3 -5]; %Enter cost
+A=[-1 -3;-1 -1]; %Enter coefficient matrix
+b=[-3;-2]; %Enter RHS column vector
+variables=["x1","x2","s1","s2","Sol"];
+[m,n]=size(A);
+s=eye(m);
+Z=[Z zeros(1,m) 0];
+A=[A s b]; 
+
+BV=[];
+
+for j=1:n
+    for i=1:m+n+1
+        if A(:,i)==s(:,j)
+            BV=[BV i];
+        end
+    end
+end
+
+fprintf("Basic Variables:\n");
+disp(variables(BV));
+
+run=true;
+while run
+    ZjCj=Z(BV)*A-Z;
+    table=[ZjCj;A];
+    simplexTable=array2table(table);
+    simplexTable.Properties.VariableNames(1:size(simplexTable,2))=variables;
+    disp(simplexTable);
+    if any(A(:,end)<0)
+        fprintf("Infeasible BFS.\n");
+        [leavingVal,pivotRow]=min(A(:,end));
+        fprintf("Leaving Row=%d \n",pivotRow);
+        Row=A(pivotRow,1:end-1);
+        Zrow=ZjCj(1,1:end-1);
+        for i=1:size(Row,2)
+            if Row(i)<0
+                ratio(i)=abs(Zrow(i)./Row(i));
+            else
+                ratio(i)=inf;
+            end
+        end
+        [enter_val,pivotCol]=min(ratio);
+        BV(pivotRow)=pivotCol;
+        fprintf("New Basic Variables:\n");
+        disp(variables(BV));
+        pivotVal=A(pivotRow,pivotCol);
+        A(pivotRow,:)=A(pivotRow,:)./pivotVal;
+        for i=1:size(A,1)
+            if i~=pivotRow
+                A(i,:)=A(i,:)-A(i,pivotCol).*A(pivotRow,:);
+            end
+            ZjCj=ZjCj-ZjCj(pivotCol).*A(pivotRow,:);
+        end
+    else
+        run=false;
+        fprintf("Feasible BFS.\n");
+    end
+end
